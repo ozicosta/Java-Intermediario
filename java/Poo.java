6ebbf4edb1809dc80b3b182f04a41bfcc92bda44
ozicosta <ozieni.costa@gmail.com>
2024-05-21 21:16:53 -0300

    enviando commit

---
+++ java/Poo.java
@@ -0,0 +1,21 @@
+package java;
+
+public class Poo {
+    //atributos da classe Poo
+   // public Boolean window;
+   // public String Movie;
+   // public Boolean food;
+
+    public  void ClosedWindow(){
+        System.out.println("Janela fechada!");
+    }
+
+    public  void WhatMovie(){
+        System.out.println("Movie: ");
+    }
+
+    public  void WhatFood(){
+        System.out.println(" Pedir comida");
+
+    }
+}
--- java/main.java
+++ java/main.java
@@ -1,7 +1,19 @@
 package java;
+ 
+ /* Java usa Programação Orientada a Objetos (POO), um estilo de programação
+que se destina a tornar o pensamento sobre programação mais próximo do pensamento sobre o mundo real.
+Na POO, cada objeto é uma unidade independente com uma identidade única,
+assim como os objetos no mundo real são. */
+
+/* na programação orientada a objetos, cada objeto tem três dimensões: identidade, atributos e comportamento.
+Atributos descrevem o estado atual do objeto, e o que o objeto é capaz de fazer é demonstrado através do comportamento do objeto. */
+
 class Main{
     public static void main(String[] args) {
-        Study l1 = new Study();
-        l1.read();
+        Poo test1 = new Poo();
+
+        test1.WhatFood();
+        test1.ClosedWindow();
+        test1.WhatMovie();
    }
 }
